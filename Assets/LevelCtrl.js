2ee7ca556762a48da1c0bf51f52f42785e66a62a
Eric Liao <rcliao01@gmail.com>
2013-08-06 02:57:41 -0700

    basic grid level with toggle

---
+++ Assets/LevelCtrl.js
@@ -0,0 +1,49 @@
+#pragma strict
+
+var hoverMat : Material;
+var selectedMat: Material;
+var originalMat : Material;
+var placementLayerMask : LayerMask;
+
+var lastHitObj : GameObject;
+
+
+function Start () {
+	
+}
+
+function Update () {
+	var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	var hit : RaycastHit;
+
+	if (Physics.Raycast(ray, hit, 1000, placementLayerMask)) {
+		if (lastHitObj) {
+			if (lastHitObj.tag == "Grid_Empty")
+				lastHitObj.renderer.material = originalMat;
+		}
+
+		lastHitObj = hit.collider.gameObject;
+
+		if (lastHitObj.tag == "Grid_Empty")
+			lastHitObj.renderer.material = hoverMat;
+	} else {
+		if (lastHitObj) {
+			lastHitObj.renderer.material = originalMat;
+			lastHitObj = null;
+		}
+	}
+
+	if (Input.GetMouseButtonDown(0) && lastHitObj) {
+		if (lastHitObj.tag == "Grid_Empty") {
+			lastHitObj.renderer.material = selectedMat;
+			lastHitObj.tag = "Grid_Taken";
+		}
+	}
+
+	if (Input.GetMouseButtonDown(1) && lastHitObj) {
+		if (lastHitObj.tag == "Grid_Taken") {
+			lastHitObj.renderer.material = originalMat;
+			lastHitObj.tag = "Grid_Empty";
+		}	
+	}
+}
